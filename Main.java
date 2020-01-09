8a779709fa66c92f630c923af362234e68240117
Benjamin Groman <benp.groman@gmail.com>
2020-01-09 15:45:11 -0500

    Now builds frame.

--- Main.java
+++ Main.java
@@ -1,8 +1,15 @@
+import javax.swing.JFrame;
 
 public class Main {
 
 	public static void main(String[] args) {
 		//build frame
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		window.setTitle("Computer Science Association Events");
+		window.setUndecorated(true);
+		window.setVisible(true);
 		//grab file
 		//hand to parser
 		//get back arraylist
