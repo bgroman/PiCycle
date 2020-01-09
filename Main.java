b782a05ac130fe8199a2a6702e9d3bf95f13672a
Benjamin Groman <benp.groman@gmail.com>
2020-01-09 15:25:54 -0500

    Basic pseudocode for main function.

---
+++ Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//build frame
+		//grab file
+		//hand to parser
+		//get back arraylist
+		//while not interrupted
+		 //foreach
+		  //update
+		  //set display panel
+		  //wait
+
+	}
+
+}
