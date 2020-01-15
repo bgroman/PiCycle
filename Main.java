b1722fb182071c88c345e982bdbe3366aec51838
Benjamin Groman <benp.groman@gmail.com>
2020-01-15 12:19:36 -0500

    Now does proper cycling.

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
@@ -15,22 +16,44 @@ public class Main {
 		window.setTitle("Computer Science Association Events");
 		window.setUndecorated(true);
 		//temporary display
-		try {
+		/*try {
 			BufferedImage screenshot = ImageIO.read(new File("screenshot.jpg"));
 			window.add(new PiSlide(new PiTimer(0, 4, 16, 0), screenshot));
 		}
 		catch (IOException e) {System.out.println("screenshot.jpg failed to load.");}
+		*/
 		window.setVisible(true);
 		
-		//grab file
-		//hand to parser
+		//grab file - skipping
+		//hand to parser - skipping
 		//get back arraylist
+		ArrayList<PiSlide> slides = new DeckBuilder().buildDeck();
 		//while not interrupted
-		 //foreach
-		  //update
-		  //set display panel
-		  //wait
-
+		while (!Thread.interrupted()) {
+			//clean slides
+			slides.removeIf(slide -> slide.targetReached());
+			//in case we run out of slides that are current
+			if (slides.size() == 0) {
+				window.dispose();
+				Thread.currentThread().interrupt();
+			}
+			//otherwise run the cycle
+			else for (PiSlide s : slides) {
+				//update
+				s.update();
+				//set display panel
+				window.setContentPane(s);
+				window.validate();
+				//wait
+				try {
+					Thread.sleep(15000);
+				}
+				catch(InterruptedException e) {
+					Thread.currentThread().interrupt();
+					break;
+				}
+			}
+		}
 	}
 
 }
